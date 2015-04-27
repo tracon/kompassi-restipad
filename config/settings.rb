660bbc1cc90047011694fc982ae056e457cdd4f2
Pekka Wallendahl <wyrmiyu@gmail.com>
2015-04-27 10:01:54 +0300

    Clean up settings-file, added LDAP_

--- config/settings.rb
+++ config/settings.rb
@@ -8,35 +8,23 @@ end
 
 # Put your configuration key = value pairs here,
 # prefix them with 'configatron.', for example: configatron.foo = 'bar'
+
+configatron.IPA_JSONRPC = 'https://moukari.tracon.fi/ipa/json'
+#configatron.IPA_CACERT_PATH = '/etc/ipa/ca.crt'
+configatron.IPA_CACERT_PATH = "#{ENV['HOME']}/.ipa/ca.crt"
+configatron.IPA_SSL_VERSION = :TLSv1
+
 configatron.LDAP_DOMAIN = 'dc=tracon,dc=fi'
 configatron.LDAP_USERS = "cn=users,cn=accounts,#{configatron.LDAP_DOMAIN}"
 configatron.LDAP_GROUPS = "cn=groups,cn=accounts,#{configatron.LDAP_DOMAIN}"
 
-configatron.IPA_JSONRPC = 'https://moukari.tracon.fi/ipa/json'
-
-#configatron.IPA_CACERT_PATH = '/etc/ipa/ca.crt'
-configatron.IPA_CACERT_PATH = "#{ENV['HOME']}/.ipa/ca.crt"
-configatron.SSL_VERSION = :TLSv1
-
 configatron.LDAP_SERVER_URI = 'ldaps://localhost:62636'
 #configatron.LDAP_SERVER_URI = 'ldaps://moukari.tracon.fi'
+configatron.LDAP_SERVER = 'localhost'
+configatron.LDAP_SERVER_PORT = 62636
 
-
-# XXX Pasted from the orginal python settings, still unsure if needed
-#
-# configatron.LDAP_BIND_DN = ''
-# configatron.LDAP_BIND_PASSWORD = ''
-# configatron.LDAP_USER_DN_TEMPLATE = 'uid=#{user},#{configatron.LDAP_USERS}'
-# configatron.LDAP_USER_SEARCH = LDAPSearch(
-#     KOMPASSI_LDAP_USERS,
-#     ldap.SCOPE_SUBTREE,
-#     '(uid=%(user)s)'
-# )
-# configatron.LDAP_ALWAYS_UPDATE_USER = True
-# configatron.LDAP_GROUP_SEARCH = LDAPSearch(
-#     KOMPASSI_LDAP_GROUPS,
-#     ldap.SCOPE_SUBTREE,
-#     '(objectClass=groupOfNames)'
-# )
-# configatron.LDAP_GROUP_TYPE = NestedGroupOfNamesType()
-# configatron.LDAP_MIRROR_GROUPS = True
+configatron.LDAP_ENCRYPTION_METHOD = :simple_tls
+configatron.LDAP_AUTH_METHOD = :simple
+configatron.LDAP_BIND_DN = 'cn=Directory Manager'
+configatron.LDAP_BIND_PASSWORD = File.open( "#{ENV['HOME']}/.ipa/dmpass",
+                                            &:readline).strip
